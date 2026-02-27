c1e9f66ea1a0471c90d7de4f02f7c3d13fa080b7
jan.thomaas <scaria.jan.thomas@gmail.com>
2026-02-27 22:36:00 +0530

    main update.

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ status_codes = [200, 404, 500, 201, 403, "oops", "nil"]
 def evaluate_build(status_code):
     if not isinstance(status_code, int):
         return {"error": "Invalid input"}
+
+
+version = 15.0
+
+if version >= 14.0:
+    if version >= 16.0:
+        print("Latest")
+    else:
+        print("Supported but not latest")
