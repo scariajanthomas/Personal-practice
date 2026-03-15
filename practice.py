2c09642ce5d26e987e528dc156dc5058a4795afd
jan.thomaas <scaria.jan.thomas@gmail.com>
2026-03-15 22:29:56 +0530

    Updating Progresss

---
+++ practice.py
@@ -0,0 +1,19 @@
+
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def display(self):
+        print(f"Username: {self.username}")
+        print(f"Password: {self.password}")
+    def validate()
+        if 
+
+
+
+user1 = User("john", "secret123")
+user1.display()
+
+
